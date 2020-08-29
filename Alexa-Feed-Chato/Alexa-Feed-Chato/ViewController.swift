e06c6e2a8414a8e008db7eeb0f790d4c455a925b
Abigail Plata <abigailplataa@gmail.com>
2020-08-28 18:59:57 -0700

    importing UI code

---
+++ Alexa-Feed-Chato/Alexa-Feed-Chato/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alexa-Feed-Chato
+//
+//  Created by Abigail Plata on 8/28/20.
+//  Copyright © 2020 Abigail Plata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
